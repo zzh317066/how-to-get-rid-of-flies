9f7b2c89146ce0ab6179a2d1888c3d362fc5933a
yanhuo <zzh317066@gmail.com>
2026-03-06 09:10:52 +0800

    chore: add robots.txt and optimize sitemap lastmod

--- site/src/app/robots.ts
+++ site/src/app/robots.ts
@@ -2,11 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/personalized-plan/result/'],
-        },
+        rules: { userAgent: '*', allow: '/' },
         sitemap: 'https://howtogetridofflies.top/sitemap.xml',
     };
 }
